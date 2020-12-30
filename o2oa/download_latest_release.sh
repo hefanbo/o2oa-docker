0fc68c52617c9dccc82548c22d5f7354b5cbacd5
Fanbo He <hefanbo@gmail.com>
2020-12-30 21:46:31 +0800

    Update download script

--- o2oa/download_latest_release.sh
+++ o2oa/download_latest_release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-URL=$(wget -q -O - http://www.o2oa.net/webSite/history.json | sed -n '0,/.*url.*\(http.*linux\.zip\).*/s//\1/p')
+URL=$(wget -q -O - http://www.o2oa.net/webSite/history.json | sed -n '0,/.*url.*\(http.*linux.*\.zip\).*/s//\1/p')
 FILE=$(echo $URL | sed -n 's/.*\/\(.*\)/\1/p')
 wget -P install_files $URL
